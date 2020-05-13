1f920c7439f77dc3499ea014be4fea478430df41
Adam Caron <adamwcaron@gmail.com>
2020-05-13 15:09:25 -0400

    created object store for 'products'

--- indexed-db-lab/app/js/main.js
+++ indexed-db-lab/app/js/main.js
@@ -21,7 +21,22 @@ var idbApp = (function() {
     return
   }
 
-  var dbPromise = idb.open('couches-n-things', 1)
+  var dbPromise = idb.open('couches-n-things', 2, function(upgradeDb) {
+    switch (upgradeDb.oldVersion) {
+      case 0:
+        // placeholder so the switch executes with the db is first created
+        // (oldVersion is 0)
+      case 1:
+        console.log('Creating the products object store')
+        upgradeDb.createObjectStore('products', { keyPath: 'id' })
+
+      // TODO 4.1 - create 'name' index
+
+      // TODO 4.2 - create 'price' and 'description' indexes
+
+      // TODO 5.1 - create an 'orders' object store
+    }
+  })
 
   function addProducts() {
 
